5ae5eeb5daa83582b6a982fa7f5ca74d8e7c1e3e
Yash L <147707204+YashL3616@users.noreply.github.com>
2024-07-23 20:13:33 +0530

    Update Power.cpp

--- 5_Recursion/Power.cpp
+++ 5_Recursion/Power.cpp
@@ -21,3 +21,38 @@ int main()
     cout<<Pow(a,n)<<" ";
     return 0;
 }
+//O(n)  TC
+
+//Less tc
+
+
+#include<iostream>
+using namespace std;
+int Pow(int a,int n){
+
+
+     if(n==1){
+        return a;
+    }
+     else if(n==0){
+        return 1;
+    }
+
+    int temp=Pow(a,n/2);
+
+    if(n%2==1){  //eg 2 power 17..calculate till 2 power 16 using above and multiply a
+     return temp*temp*a;
+    }
+    else return temp*temp;
+
+   
+}
+int main()
+{
+    int a,n;
+    cout<<"Enter base and power"<<endl;
+    cin>>a>>n;
+    cout<<"The Answer is :";
+    cout<<Pow(a,n)<<" ";
+    return 0;
+}
