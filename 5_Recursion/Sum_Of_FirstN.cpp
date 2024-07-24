6599819fbe3d7496fffb43f90558a978cf450b72
Yash L <147707204+YashL3616@users.noreply.github.com>
2024-07-24 16:03:14 +0530

    Update Sum_Of_FirstN.cpp

--- 5_Recursion/Sum_Of_FirstN.cpp
+++ 5_Recursion/Sum_Of_FirstN.cpp
@@ -1,3 +1,20 @@
+//basic
+#include<bits/stdc++.h>
+using namespace std;
+ void solve(int N) {
+    int sum = N * (N + 1) / 2;
+    cout<<"The sum of the first "<<N<<" numbers is: "<<sum<<endl;
+  }
+ int main() {
+
+    solve(5);
+    solve(6);
+  }
+
+
+
+
+//Nishant Chahar
 #include<iostream>
 using namespace std;
 int sumRec(int N)
@@ -5,7 +22,7 @@ int sumRec(int N)
     if(N==1){  //base think of smallest sumRec possibility Input N=1
     return 1;
     }
-int temp=sumRec(N-1);  //smalles subproblem closes to N is N-1
+int temp=sumRec(N-1);  //smallest subproblem closes to N is N-1
 return (temp+N);   //sumRec(N-1)+N is ans
 }
 int main()
@@ -17,4 +34,34 @@ int main()
     cout<<"The sum of first "<<N<<" Natural Numbers is :"<<res<<endl;
     return 0;
   
+}
+
+
+
+
+//Parametrized..ake u forward
+#include<bits/stdc++.h>
+using namespace std;
+
+void func(int i, int sum){
+   
+   // Base Condition.
+   if(i<1)
+   {
+       cout<<sum<<endl;
+       return;
+   }
+
+   // Function call to increment sum by i till i decrements to 1.
+   func(i-1,sum+i);
+
+}
+
+int main(){
+  
+  // Here, let’s take the value of n to be 3.
+  int n = 3;
+  func(n,0);
+  return 0;
+
 }
