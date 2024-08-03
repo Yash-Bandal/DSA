3712b48c226acc5150a540bd476bf7be1981e6e1
Yash L <147707204+YashL3616@users.noreply.github.com>
2024-08-03 17:12:14 +0530

    Update MapHashing.cpp

--- 6 Hashing/MapHashing.cpp
+++ 6 Hashing/MapHashing.cpp
@@ -89,3 +89,29 @@ int main()
     }
     return 0;
 }
+
+
+//Main 
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void Frequency(int arr[], int n)
+{
+    unordered_map<int, int> map;
+ 
+    for (int i = 0; i < n; i++)
+        map[arr[i]]++;
+ 
+    // Traverse through map and print frequencies
+    for (auto it : map)
+        cout <<it.first << " :" << it.second << endl;
+}
+ 
+int main()
+{
+    int arr[] = {10,5,10,15,10,5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    Frequency(arr, n);
+    return 0;
+}
