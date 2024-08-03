2bc24f29b1958a80fe2c705458ac87ca780c8947
Yash L <147707204+YashL3616@users.noreply.github.com>
2024-08-03 22:43:02 +0530

    Update MinMaxFreq_Map.cpp

--- 6 Hashing/MinMaxFreq_Map.cpp
+++ 6 Hashing/MinMaxFreq_Map.cpp
@@ -1,3 +1,6 @@
+//Passing array
+
+
 #include <bits/stdc++.h>
 #include <climits>
 using namespace std;
@@ -75,3 +78,67 @@ int main()
     countFreq(arr, n);
     return 0;
 }
+
+
+
+// Passing a vector
+#include <bits/stdc++.h>
+#include <climits>
+using namespace std;
+
+void countFreq(vector<int> vec, int n)
+{
+    unordered_map<int, int> mpp;
+
+    int maxFreq = INT_MIN; // not INT_MAX for max
+    vector<int> maxFreqEle;
+
+    int minFreq = INT_MAX; // not INT_MIN for min
+    vector<int> minFreqEle;
+
+    for (size_t i = 0; i < n; i++)
+    {
+        mpp[vec[i]]++;
+    }
+
+    for (auto it : mpp)
+    {
+        cout << it.first << " :" << it.second << endl;
+    }
+    for (auto it : mpp)
+    {
+        maxFreq = max(maxFreq, it.second);
+        minFreq = min(minFreq, it.second);
+    }
+    for (auto it : mpp)
+    {
+        if (it.second == maxFreq)
+            maxFreqEle.push_back(it.first);
+
+        if (it.second == minFreq)
+            minFreqEle.push_back(it.first);
+    }
+
+    cout << "Element with max Freq is: ";
+    for (auto it : maxFreqEle)
+    {
+        cout << it << " ";
+    }
+    cout << "with freq " << maxFreq << endl;
+
+    cout << "Element with min Freq is: ";
+    for (auto it : minFreqEle)
+    {
+        cout << it << " ";
+    }
+    cout << "with freq " << minFreq << endl;
+}
+
+int main()
+{
+    vector<int> vec = {1, 2, 3, 3, 3, 4, 5, 6, 7, 7, 7, 0};
+    int n = vec.size();
+    countFreq(vec, n);
+    return 0;
+}
+
