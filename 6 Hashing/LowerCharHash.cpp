5f7a3971e693bbc690f3dba42591ba806b525984
Yash L <147707204+YashL3616@users.noreply.github.com>
2024-08-06 18:59:20 +0530

    Update LowerCharHash.cpp

--- 6 Hashing/LowerCharHash.cpp
+++ 6 Hashing/LowerCharHash.cpp
@@ -24,3 +24,25 @@ cout<<"Enter queries"<<endl;
     }
     return 0;
 }
+
+
+/*See leetcode 3016*/
+class Solution {
+public:
+    int minimumPushes(string word) {
+        vector<int> letterFreq(26, 0);
+        for (char c : word) {
+            letterFreq[c - 'a']++;
+        }
+        
+        sort(letterFreq.rbegin(), letterFreq.rend());
+        
+        int totalPresses = 0;
+        for (int i = 0; i < 26; i++) {
+            if (letterFreq[i] == 0) break;
+            totalPresses += (i / 8 + 1) * letterFreq[i];
+        }
+        
+        return totalPresses;
+    }
+};
