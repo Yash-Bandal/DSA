660c8d94e06c331dfdaff09291b3bb204d756491
Yash Bandal <147707204+Yash-Bandal@users.noreply.github.com>
2024-08-08 21:39:37 +0530

    Update 6_SelectionRecursive.cpp

--- 7 Sorting/6_SelectionRecursive.cpp
+++ 7 Sorting/6_SelectionRecursive.cpp
@@ -21,12 +21,12 @@ void selectionSortRecursive(vector<int> &arr, int n, int beginIndex)
         return;
     }
     int minIndex = beginIndex;
-    for (int i = beginIndex+1; i < n; i++)
+    for (int j = beginIndex+1; j < n; j++)
     {
 
-        if (arr[i] < arr[minIndex])
+        if (arr[j] < arr[minIndex])
         {
-            minIndex = i;
+            minIndex = j;
         }
     }
     int temp = arr[minIndex];
@@ -35,6 +35,56 @@ void selectionSortRecursive(vector<int> &arr, int n, int beginIndex)
 
     // Recursively sort the remaining part of the array
     selectionSortRecursive(arr, n, beginIndex + 1);
+}
+
+void printArray(const vector<int> &arr)
+{
+    for (int num : arr)
+    {
+        cout << num << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    vector<int> arr = {7, 9, 0, 5, 3, 4, 2, 1, 6};
+    int n = arr.size();
+    selectionSortRecursive(arr, n, 0);
+    cout << "After Sorting:" << endl;
+    printArray(arr);
+    return 0;
+}
+
+
+//simply  beginIndex is like i looping
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// Recursive function to sort the array using selection sort
+void selectionSortRecursive(vector<int> &arr, int n, int i)
+{
+    // base case...index reach last
+    if (i == n - 1)
+    {
+        return;
+    }
+    int minIndex = i;
+    for (int j = i+1; j < n; j++)
+    {
+
+        if (arr[j] < arr[minIndex])
+        {
+            minIndex = j;
+        }
+    }
+    int temp = arr[minIndex];
+    arr[minIndex] = arr[i];
+    arr[i] = temp;
+
+    // Recursively sort the remaining part of the array
+    selectionSortRecursive(arr, n, i + 1);
 }
 
 void printArray(const vector<int> &arr)
