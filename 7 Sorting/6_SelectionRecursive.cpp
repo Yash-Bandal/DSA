b4cb088212a865034b4a3b33bc377618456b66c9
Yash Bandal <147707204+Yash-Bandal@users.noreply.github.com>
2024-08-08 19:11:58 +0530

    Update 6_SelectionRecursive.cpp

--- 7 Sorting/6_SelectionRecursive.cpp
+++ 7 Sorting/6_SelectionRecursive.cpp
@@ -1,41 +1,56 @@
-#include <bits/stdc++.h>
+/*
+Simply remember old non rec selectionSort
+
+we here eliminate outer loop
+
+
+let j=i here and resemble i as beginIndex
+perform the outer loop as beginIndex recursion
+*/
+
+
+#include<bits/stdc++.h>
 using namespace std;
 
 // Recursive function to sort the array using selection sort
-void selectionSortRecursive(vector<int>& arr, int n, int index = 0) {
-    // Base case: when the entire array is sorted
-    if (index == n - 1) {
+void selectionSortRecursive(vector<int> &arr, int n, int beginIndex)
+{
+    // base case...index reach last
+    if (beginIndex == n - 1)
+    {
         return;
     }
+    int minIndex = beginIndex;
+    for (int i = beginIndex+1; i < n; i++)
+    {
 
-    // Find the minimum element in the unsorted part of the array
-    int minIndex = index;
-    for (int i = index + 1; i < n; i++) {
-        if (arr[i] < arr[minIndex]) {
+        if (arr[i] < arr[minIndex])
+        {
             minIndex = i;
         }
     }
-
-    // Swap the found minimum element with the first element
-    if (minIndex != index) {
-        swap(arr[minIndex], arr[index]);
-    }
+    int temp = arr[minIndex];
+    arr[minIndex] = arr[beginIndex];
+    arr[beginIndex] = temp;
 
     // Recursively sort the remaining part of the array
-    selectionSortRecursive(arr, n, index + 1);
+    selectionSortRecursive(arr, n, beginIndex + 1);
 }
 
-void printArray(const vector<int>& arr) {
-    for (int num : arr) {
+void printArray(const vector<int> &arr)
+{
+    for (int num : arr)
+    {
         cout << num << " ";
     }
     cout << endl;
 }
 
-int main() {
+int main()
+{
     vector<int> arr = {7, 9, 0, 5, 3, 4, 2, 1, 6};
     int n = arr.size();
-    selectionSortRecursive(arr, n);
+    selectionSortRecursive(arr, n, 0);
     cout << "After Sorting:" << endl;
     printArray(arr);
     return 0;
