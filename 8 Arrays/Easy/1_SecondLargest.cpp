7be51f2dd1be850c5ded550a46947ecacbbc0574
Yash Bandal <147707204+Yash-Bandal@users.noreply.github.com>
2024-08-10 23:22:11 +0530

    Update 1_SecondLargest.cpp

--- 8 Arrays/Easy/1_SecondLargest.cpp
+++ 8 Arrays/Easy/1_SecondLargest.cpp
@@ -1,1 +1,57 @@
+//Largest
+// Brute Force Sort
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int sortArr(vector<int>& arr) {
+    sort(arr.begin(),arr.end());
+    return arr[arr.size()-1];
+}
+ 
+int main() {
+    vector<int> arr1 = {2,5,1,3,0};
+    vector<int> arr2 = {8,10,5,7,9};
+   
+    cout<<"The Largest element in the array is: "<<sortArr(arr1)<<endl;
+    cout<<"The Largest element in the array is: "<<sortArr(arr2);
+   
+    return 0;
+}
 
+Output:
+The Largest element in the array is: 5
+The Largest element in the array is: 10
+
+Time Complexity: O(N*log(N))
+Space Complexity: O(n)
+
+  
+
+//Using max var
+ #include <bits/stdc++.h>
+ 
+using namespace std;
+int findLargestElement(int arr[], int n) {
+ 
+  int max = arr[0];
+  for (int i = 0; i < n; i++) {
+    if (max < arr[i]) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
+int main() {
+  int arr1[] = {2,5,1,3,0};
+  int n = 5;
+  int max = findLargestElement(arr1, n);
+  cout << "The largest element in the array is: " << max << endl;
+ 
+  int arr2[] =  {8,10,5,7,9};
+  n = 5;
+  max = findLargestElement(arr2, n);
+  cout << "The largest element in the array is: " << max<<endl;
+  return 0;
+}
+Time Complexity: O(N)
+Space Complexity: O(1)
