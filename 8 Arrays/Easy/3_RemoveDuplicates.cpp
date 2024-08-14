b5ce93d6bdebc3ff59b0dd60489566e373eab7f0
Yash Bandal <147707204+Yash-Bandal@users.noreply.github.com>
2024-08-14 21:42:56 +0530

    Update 3_RemoveDuplicates.cpp

--- 8 Arrays/Easy/3_RemoveDuplicates.cpp
+++ 8 Arrays/Easy/3_RemoveDuplicates.cpp
@@ -64,3 +64,55 @@ int main() {
     cout << arr[i] << " ";
   }
 }
+
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int removeDuplicates(int *arr, int n) {
+  set<int> set;
+
+  for (int i = 0; i < n; i++) {
+    set.insert(arr[i]);
+  }
+  int setSz = set.size();
+  int j = 0;
+  for (int x : set) {
+    arr[j++] = x;
+  }
+  return setSz;
+}
+
+// Two pointer method
+int removeDuplicates2(int *arr, int n) {
+  if (n == 0) return 0;  // edge case for empty array
+  int i = 0;
+  for (int j = 1; j < n; j++) {
+    if (arr[j] != arr[i]) {
+      arr[++i] = arr[j];
+    }
+  }
+  return i + 1;
+}
+
+int main() {
+  int arr[] = {1, 1, 2, 2, 2, 3, 3};
+  int n = sizeof(arr) / sizeof(arr[0]);
+
+  int arr2[] = {1, 1, 2, 2, 2, 3, 3, 4, 4, 4};
+  int N = sizeof(arr2) / sizeof(arr2[0]);
+
+  int k = removeDuplicates(arr, n);
+  cout << "The array after removing duplicate elements is " << endl;
+  for (int i = 0; i < k; i++) {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+
+  int m = removeDuplicates2(arr2, N); // use N instead of n here
+  cout << "The array after removing duplicate elements using the two-pointer technique is " << endl;
+  for (int i = 0; i < m; i++) {
+    cout << arr2[i] << " ";
+  }
+}
+
