c37f6ddb3f87ca29de786894e06f70bf62bb917f
Yash Bandal <147707204+Yash-Bandal@users.noreply.github.com>
2024-08-22 18:02:08 +0530

    Update All_15.py

--- LeetCode/Maths/Pandas/All_15.py
+++ LeetCode/Maths/Pandas/All_15.py
@@ -5,3 +5,10 @@ def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
     df=pd.DataFrame(student_data,columns=["student_id","age"])
     return df;
 
+#2878. Get the Size of a DataFrame
+import pandas as pd
+
+def getDataframeSize(players: pd.DataFrame) -> List[int]:
+    row_count=players.shape[0]
+    col_count=players.shape[1]
+    return [row_count,col_count]
