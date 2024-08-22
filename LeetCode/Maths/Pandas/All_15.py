5b661d99eddb35da97f06bb9144936eeda68c310
Yash Bandal <147707204+Yash-Bandal@users.noreply.github.com>
2024-08-22 18:01:28 +0530

    Update All_15.py

--- LeetCode/Maths/Pandas/All_15.py
+++ LeetCode/Maths/Pandas/All_15.py
@@ -1,1 +1,7 @@
+#2877. Create a DataFrame from List
+import pandas as pd
+
+def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
+    df=pd.DataFrame(student_data,columns=["student_id","age"])
+    return df;
 
