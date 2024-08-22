ef14808e96352c46c8fc6bf9facec85c6d770931
Yash Bandal <147707204+Yash-Bandal@users.noreply.github.com>
2024-08-22 18:03:14 +0530

    Update All_15.py

--- LeetCode/Maths/Pandas/All_15.py
+++ LeetCode/Maths/Pandas/All_15.py
@@ -19,3 +19,14 @@ import pandas as pd
 def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
     df=employees.head(3)
     return df
+
+#2880. Select Data
+import pandas as pd
+
+def selectData(students: pd.DataFrame) -> pd.DataFrame:
+    # return students.loc[students["student_id"] == 101, ["name", "age"]]
+    # # OR
+     return students.loc[students["student_id"] == 101, "name" :]
+    # # OR
+    # return students[students['student_id'] == 101][['name', 'age']]:
+    
