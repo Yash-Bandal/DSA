98a3bbc2f0633571a2c1f532cb4b8c89cac963c8
Yash Bandal <147707204+Yash-Bandal@users.noreply.github.com>
2024-08-23 07:46:03 +0530

    Update 12_MissingNumber.cpp

--- 8 Arrays/Easy/12_MissingNumber.cpp
+++ 8 Arrays/Easy/12_MissingNumber.cpp
@@ -1,18 +1,21 @@
 //Brute Force
-
+/*
+The inner loop uses N-1 instead of N because the array a 
+contains only N-1 elements (as one number is missing). Therefore, we iterate over the indices 0 to N-2
+*/
 #include <bits/stdc++.h>
 using namespace std;
 int missingNumber(vector<int>&a, int N) {
 
     // Outer loop that runs from 1 to N:
-    for (int i = 1; i <= N; i++) {
+    for (int i = 1; i <= N+1; i++) {   //
 
         // flag variable to check
         //if an element exists
         int flag = 0;
 
         //Search the element using linear search:
-        for (int j = 0; j < N - 1; j++) {
+        for (int j = 0; j < N ; j++) {          
             if (a[j] == i) {   //not arr[i]  its i
 
                 // i is present in the array:
@@ -34,8 +37,8 @@ int missingNumber(vector<int>&a, int N) {
 
 int main()
 {
-    int N = 5;
-    vector<int> a = {1, 2, 4, 5};
+    vector<int> a = {1, 2, 3,4};
+    int N=a.size();
     int ans = missingNumber(a, N);
     cout << "The missing number is: " << ans << endl;
     return 0;
