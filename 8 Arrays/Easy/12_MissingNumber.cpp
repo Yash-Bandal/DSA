0520c587ecb8dc3e460dafd555a1a294640b98a3
Yash Bandal <147707204+Yash-Bandal@users.noreply.github.com>
2024-08-24 10:02:55 +0530

    Update 12_MissingNumber.cpp

--- 8 Arrays/Easy/12_MissingNumber.cpp
+++ 8 Arrays/Easy/12_MissingNumber.cpp
@@ -153,3 +153,29 @@ int main()
 /* Takes TC=O(2*N)  reduce it by using single loop */  
 
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int missingNumber(vector<int>&a, int N) {
+
+    int xor1 = 0, xor2 = 0;
+
+//itwrate through array and later for xor1 which need xtra included missing num..xor1 ^with N..the last num
+    for (int i = 0; i < N - 1; i++) {
+        xor2 = xor2 ^ a[i]; // XOR of array elements
+        xor1 = xor1 ^ (i + 1); //XOR up to [1...N-1]
+    }
+    xor1 = xor1 ^ N; //XOR up to [1...N]
+
+    return (xor1 ^ xor2); // the missing number
+}
+
+int main()
+{
+    int N = 5;
+    vector<int> a = {1, 2, 4, 5};
+    int ans = missingNumber(a, N);
+    cout << "The missing number is: " << ans << endl;
+    return 0;
+}
+/*Takes TC=O(N)*/
