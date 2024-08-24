8a85eb685d864a8f7a3960368dbc75e18e058cfe
Yash Bandal <147707204+Yash-Bandal@users.noreply.github.com>
2024-08-24 10:02:33 +0530

    Update 12_MissingNumber.cpp

--- 8 Arrays/Easy/12_MissingNumber.cpp
+++ 8 Arrays/Easy/12_MissingNumber.cpp
@@ -71,7 +71,6 @@ int missingNumber(vector<int>&a, int N) {
 
 int main()
 {
-   
     vector<int> a = {1, 2, 3,4};
     int N=a.size();
     int ans = missingNumber(a, N);
@@ -100,7 +99,7 @@ using namespace std;
 int missingNumber(vector<int>&a, int N) {
 
     //Summation of first N numbers:
-    int sum = (N * (N + 1)) / 2;
+    int sum = (N * (N + 1)) / 2;  
 
     //Summation of all array elements:
     int s2 = 0;
@@ -120,5 +119,37 @@ int main()
     cout << "The missing number is: " << ans << endl;
     return 0;
 }
+
+//Optimal 2 - XOR
+#include <bits/stdc++.h>
+using namespace std;
+
+int missingNumber(vector<int> &a, int N)
+{
+
+    int xor1 = 0, xor2 = 0;
+
+    // Run Xor for complete set of array without missing num    ..1 to N
+    for (int i = 1; i <= N; i++)
+    {
+        xor1 = xor1 ^ i;
+    }
+    // Run Xor for complete set of array with missing num     ..index 0 to N-1 n is not size here observe
+    for (int i = 0; i < N - 1; i++) // array iteration
+    {
+        xor2 = xor2 ^ a[i];
+    }
+    return xor1 ^ xor2;
+}
+
+int main()
+{
+    int N = 5;
+    vector<int> a = {1, 2, 4, 5};
+    int ans = missingNumber(a, N);
+    cout << "The missing number is: " << ans << endl;
+    return 0;
+}
+/* Takes TC=O(2*N)  reduce it by using single loop */  
 
 
