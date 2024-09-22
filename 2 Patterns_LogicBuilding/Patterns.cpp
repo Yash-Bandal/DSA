0b3617433a8ccd18934166290ecf51c0e964a228
Yash Bandal <147707204+Yash-Bandal@users.noreply.github.com>
2024-09-22 22:50:26 +0530

    Update Patterns.cpp

--- 2 Patterns_LogicBuilding/Patterns.cpp
+++ 2 Patterns_LogicBuilding/Patterns.cpp
@@ -282,12 +282,12 @@ void printPattern13_1(int n)
 
 void printPattern13_2(int n)
 {
-    for (int i = 1; i <= (2 * n - 1); i++)
+    for (int i = 1; i <= (2 * n - 1); i++)  //n-one not i
     {
         int stars = i;
         if (i > n)
         {
-            stars = (2 * n - i);
+            stars = (2 * n - i);  //n- i not one
         };
 
         for (int j = 1; j <= stars; j++)
