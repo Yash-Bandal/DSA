0e433e143f4bda7e3abe656b75c9730f52432ee3
Yash Bandal <147707204+Yash-Bandal@users.noreply.github.com>
2024-09-23 07:35:50 +0530

    Create 5_BuySell._DP_1.cpp

---
+++ 8 Arrays/Medium/5_BuySell._DP_1.cpp
@@ -0,0 +1,67 @@
+//Brute..irst show this as one may ask ..how can you do without optimize
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxProfit(vector<int> &arr) {
+    int maxPro = 0;
+    int n = arr.size();
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] > arr[i]) {
+            maxPro = max(arr[j] - arr[i], maxPro);
+            }
+        }
+        }
+
+    return maxPro;
+}
+
+int main() {
+    vector<int> arr = {7,1,5,3,6,4};
+    int maxPro = maxProfit(arr);
+    cout << "Max profit is: " << maxPro << endl;
+}
+
+//Opt
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxProfit(vector<int> &arr) {
+    int maxPro = 0;
+    int n = arr.size();
+    int minPrice = INT_MAX;
+
+    for (int i = 0; i < arr.size(); i++) {
+        minPrice = min(minPrice, arr[i]);
+        maxPro = max(maxPro, arr[i] - minPrice);
+    }
+    
+    return maxPro;
+}
+
+int main() {
+    vector<int> arr = {7,1,5,3,6,4};
+    int maxPro = maxProfit(arr);
+    cout << "Max profit is: " << maxPro << endl;
+}
+
+//or 
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minI= prices[0];
+        int cost=0;
+        int profit = INT_MIN;  //- inf
+        for(int i = 0 ; i < prices.size() ; i++){
+            cost = prices[i] - minI;
+            profit = max(cost,profit);
+            minI=min(minI,prices[i]);
+        }
+        return profit;
+        
+    }
+};
+//
