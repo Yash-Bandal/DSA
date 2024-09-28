ddf0d4fb1de795add03811ebe1b9986fbc7423d1
Yash Bandal <147707204+Yash-Bandal@users.noreply.github.com>
2024-09-28 21:25:58 +0530

    Update 73. Set Matrix Zeroes.cpp

--- LeetCode/Arrays/73. Set Matrix Zeroes.cpp
+++ LeetCode/Arrays/73. Set Matrix Zeroes.cpp
@@ -43,3 +43,38 @@ public:
         }
     }
 };
+
+
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        // dont create arrays
+        // int row[] = {0};
+        // int col[m]={0};
+
+        // create vectors instead of arrays
+        vector<int> row(n, 0); // initialize a vector of size n with 0
+        vector<int> col(m, 0); // initialize a vector of size m with 0
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+        // reiterate
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (col[j] == 1 || row[i] == 1) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
