74cd4afb9ef6f672f185c0e33e1cd7e8796e47e8
Yash Bandal <147707204+Yash-Bandal@users.noreply.github.com>
2024-10-01 23:35:08 +0530

    Create 14_SubarraySumCountk.cpp

---
+++ 8 Arrays/Medium/14_SubarraySumCountk.cpp
@@ -0,0 +1,124 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int cnt = 0;
+        int preSum = 0;
+        map<int, int> mpp;
+        mpp[0] = 1;
+        for (int i = 0; i < nums.size(); i++) {
+            preSum += nums[i];
+            int remain = preSum - k;
+            cnt += mpp[remain];
+            mpp[preSum] += 1;
+        }
+        return cnt;
+    }
+};
+//less opt
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int cnt=0;
+  for (int i = 0; i < nums.size(); ++i) {
+            long long sum = 0;
+            for (int j = i; j < nums.size(); ++j) {
+                sum += nums[j];
+                if (sum == k) {
+                    cnt++;
+                }
+            }
+        }
+     return cnt;   
+    }
+};
+
+int findAllSubarraysWithGivenSum(vector < int > & arr, int k) {
+    // Write Your Code Here
+    map <int,int> mpp;
+    mpp[0]=1; //you forget this..this is very imp
+    int preSum=0;
+    int cnt=0;
+    //iterate
+    for (int i = 0; i < arr.size(); i++) {
+    preSum+=arr[i]; //first calculate presum upto index i
+    int remain=preSum-k;  //find x-k
+    cnt+=mpp[remain];  //checks remain ..ie the count present in mpp..if yes add else not add
+    mpp[preSum]+=1;
+
+
+    }
+    return cnt;
+}
+
+
+//Brute
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int findAllSubarraysWithGivenSum(vector < int > & arr, int k) {
+    int n = arr.size(); // size of the given array.
+    int cnt = 0; // Number of subarrays:
+
+    for (int i = 0 ; i < n; i++) { // starting index i
+        for (int j = i; j < n; j++) { // ending index j
+
+            // calculate the sum of subarray [i...j]
+            int sum = 0;
+            for (int K = i; K <= j; K++)
+                sum += arr[K];
+
+            // Increase the count if sum == k:
+            if (sum == k)
+                cnt++;
+        }
+    }
+    return cnt;
+}
+
+int main()
+{
+    vector arr = {3, 1, 2, 4};
+    int k = 6;
+    int cnt = findAllSubarraysWithGivenSum(arr, k);
+    cout << "The number of subarrays is: " << cnt << "\n";
+    return 0;
+}
+
+
+
+//Better
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int findAllSubarraysWithGivenSum(vector < int > & arr, int k) {
+    int n = arr.size(); // size of the given array.
+    int cnt = 0; // Number of subarrays:
+
+    for (int i = 0 ; i < n; i++) { // starting index i
+        int sum = 0;
+        for (int j = i; j < n; j++) { // ending index j
+            // calculate the sum of subarray [i...j]
+            // sum of [i..j-1] + arr[j]
+            sum += arr[j];
+
+            // Increase the count if sum == k:
+            if (sum == k)
+                cnt++;
+        }
+    }
+    return cnt;
+}
+
+int main()
+{
+    vector arr = {3, 1, 2, 4};
+    int k = 6;
+    int cnt = findAllSubarraysWithGivenSum(arr, k);
+    cout << "The number of subarrays is: " << cnt << "\n";
+    return 0;
+}
+
+
+//optim
