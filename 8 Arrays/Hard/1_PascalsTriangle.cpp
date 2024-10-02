91d3554bb6d6a7be777e0e0f19e824019d911bcc
Yash Bandal <147707204+Yash-Bandal@users.noreply.github.com>
2024-10-02 22:12:55 +0530

    Update 1_PascalsTriangle.cpp

--- 8 Arrays/Hard/1_PascalsTriangle.cpp
+++ 8 Arrays/Hard/1_PascalsTriangle.cpp
@@ -1,1 +1,30 @@
 
+//Leet
+class Solution {
+private:
+    vector<int> generateRow(int row) {
+        int ans=1;//every element
+        vector<int> ansRow;
+        // Push initial 1 ..each row has 1 first
+        ansRow.push_back(1);
+
+        // run from 1 to  row size  for each col
+        for (int col = 1; col < row; col++) {
+            ans = ans * (row - col);
+            ans = ans / col;
+            ansRow.push_back(ans);
+        }
+
+        return ansRow;
+    }
+
+public:
+    vector<vector<int>> generate(int numRows) {
+        // cretae result ans 2d vec
+        vector<vector<int>> res;
+        for (int row = 1; row <= numRows; row++){
+            res.push_back(generateRow(row));
+        }
+        return res;
+    }
+};
