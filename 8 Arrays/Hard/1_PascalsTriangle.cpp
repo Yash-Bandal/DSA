55930acf43348b249c39907bba4d4e212b6b7343
Yash Bandal <147707204+Yash-Bandal@users.noreply.github.com>
2024-10-02 22:14:20 +0530

    Update 1_PascalsTriangle.cpp

--- 8 Arrays/Hard/1_PascalsTriangle.cpp
+++ 8 Arrays/Hard/1_PascalsTriangle.cpp
@@ -61,3 +61,33 @@ int main()
 }
         
         
+//2 Given the row number n. Print the n-th row of Pascal’s triangle.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int nCr(int n, int r) {
+    long long res = 1;
+
+    // calculating nCr:
+    for (int i = 0; i < r; i++) {
+        res = res * (n - i);
+        res = res / (i + 1);
+    }
+    return res;
+}
+
+void pascalTriangle(int n) {
+    // printing the entire row n:
+    for (int c = 1; c <= n; c++) {
+        cout << nCr(n - 1, c - 1) << " ";
+    }
+    cout << "n";
+}
+
+int main()
+{
+    int n = 5;
+    pascalTriangle(n);
+    return 0;
+}
