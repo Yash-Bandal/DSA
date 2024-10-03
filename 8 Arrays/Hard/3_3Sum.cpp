731c57f6fe429cb6e0fec36612f717149f3f59af
Yash Bandal <147707204+Yash-Bandal@users.noreply.github.com>
2024-10-03 22:32:02 +0530

    Update 3_3Sum.cpp

--- 8 Arrays/Hard/3_3Sum.cpp
+++ 8 Arrays/Hard/3_3Sum.cpp
@@ -1,5 +1,95 @@
+//Leet
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& arr) {
+        int n=arr.size();
+        vector<vector<int>> ans;
+        sort(arr.begin(), arr.end());
+        for (int i = 0; i < n; i++) {
+            // remove duplicates:
+            if (i != 0 && arr[i] == arr[i - 1])
+                continue;
 
+            // moving 2 pointers:
+            int j = i + 1;
+            int k = n - 1;
+            while (j < k) {
+                int sum = arr[i] + arr[j] + arr[k];
+                if (sum < 0) {
+                    j++;
+                } else if (sum > 0) {
+                    k--;
+                } else { //when equal to target here 0
+                    vector<int> temp = {arr[i], arr[j], arr[k]};
+                    ans.push_back(temp); //push each single list/vec to 2d vec
+                    j++;
+                    k--;
+                    // skip the duplicates:
+                    while (j < k && arr[j] == arr[j - 1])
+                        j++;
+                    while (j < k && arr[k] == arr[k + 1])
+                        k--;
+                }
+            }
+        }
+        return ans;
+    }
+};
 
+//Brute Gives TLE 𝑂(𝑛3log⁡3)
+ wherever keyword uniq use set
+ class Solution {
+ public:
+     vector<vector<int>> threeSum(vector<int>& nums) {
+         set<vector<int>> st;
+         int n = nums.size();
+         vector<int> temp;
+         for (int i = 0; i < n; i++) {
+             for (int j = i + 1; j < n; j++) {
+                 for (int k = j + 1; k < n; k++) {
+                     if (nums[i] + nums[j] + nums[k] == 0) {
+                         temp = {nums[i], nums[j], nums[k]};
+                         sort(temp.begin(), temp.end());
+                         st.insert(temp);
+                     }
+                 }
+             }
+         }
+         vector<vector<int>> ans(st.begin(), st.end());
+         return ans;
+     }
+ };
+
+//Better TLE O(n sq +log M)
+ wherever keyword uniq use set //tle
+ class Solution {
+ public:
+     vector<vector<int>> threeSum(vector<int>& arr) {
+         int n=arr.size();
+  set<vector<int>> st;
+
+    for (int i = 0; i < n; i++) {
+        set<int> hashset;
+        for (int j = i + 1; j < n; j++) {
+            //Calculate the 3rd element:
+            int third = -(arr[i] + arr[j]);
+
+            //Find the element in the set:
+            if (hashset.find(third) != hashset.end()) {
+                vector<int> temp = {arr[i], arr[j], third};
+                sort(temp.begin(), temp.end());
+                st.insert(temp);
+            }
+            hashset.insert(arr[j]);
+        }
+    }
+
+    //store the set in the answer:
+    vector<vector<int>> ans(st.begin(), st.end());
+    return ans;
+    }
+};
+////////////////////////////////////////////////////////////////////////
 //Brute TLE error
 #include <bits/stdc++.h>
 using namespace std;
