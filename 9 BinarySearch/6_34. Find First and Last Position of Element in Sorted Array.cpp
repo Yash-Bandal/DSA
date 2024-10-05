45562f440046c7a7a81ea4e000856b6a6acb62a6
Yash Bandal <147707204+Yash-Bandal@users.noreply.github.com>
2024-10-05 20:05:16 +0530

    Update 6_34. Find First and Last Position of Element in Sorted Array.cpp

--- 9 BinarySearch/6_34. Find First and Last Position of Element in Sorted Array.cpp
+++ 9 BinarySearch/6_34. Find First and Last Position of Element in Sorted Array.cpp
@@ -26,32 +26,6 @@ public:
 };
 
 
-//O(n)
-// class Solution {
-// public:
-//     vector<int> searchRange(vector<int>& nums, int target) {
-//  int n = nums.size();
-//     int left = 0, right = n - 1;
-    
-//     // Find the starting position
-//     while (left <= right && nums[left] != target) {
-//         left++;
-//     }
-
-//     // Find the ending position
-//     while (left <= right && nums[right] != target) {
-//         right--;
-//     }
-
-//     // If the target is not found, return [-1, -1]
-//     if (left > right) {
-//         return {-1, -1};
-//     }
-
-//     return {left, right};
-//     }
-// };
-
 //Linear search  Linear search
 class Solution {
 public:
