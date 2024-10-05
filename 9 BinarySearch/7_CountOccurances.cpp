a05e8bd4eac265c3d3e3ff9bb28cdc9667d2418d
Yash Bandal <147707204+Yash-Bandal@users.noreply.github.com>
2024-10-05 21:40:14 +0530

    Create 7_CountOccurances.cpp

---
+++ 9 BinarySearch/7_CountOccurances.cpp
@@ -0,0 +1,56 @@
+//Geeks
+class Solution{
+     private:
+    //first occ
+    int firstOcc(int arr[], int x, int n) {
+        int low = 0, high = n - 1;
+        int first = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            // maybe an answer
+            if (arr[mid] == x) {
+                first = mid;
+              //eliminate right
+                high = mid - 1;
+            } else if(arr[mid] < x) {
+                low = mid + 1; // look on the right
+            }
+            else {
+                high=mid-1;
+            }
+        }
+        return first;
+    }
+    //last occ
+        int lastOcc(int  arr[], int x, int n) {
+        int low = 0, high = n - 1;
+        int last = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            // maybe an answer
+            if (arr[mid] == x) {
+                last = mid;
+              //eliminate right
+                low = mid + 1;
+            } else if(arr[mid] < x) {
+                low = mid + 1; // look on the right
+            }
+            else {
+                high=mid-1;
+            }
+        }
+        return last;
+    }
+public:	
+	/* if x is present in arr[] then returns the count
+		of occurrences of x, otherwise returns 0. */
+	int count(int arr[], int n, int x) {
+	    int first=firstOcc(arr,x,n);
+        if(first == -1){return 0;}
+        int last=lastOcc(arr,x,n);
+        return (last-first+1);
+	    // code here
+	}
+};
