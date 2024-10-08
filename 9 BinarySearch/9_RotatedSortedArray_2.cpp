f2f7dc30b31acf6916687ec5634d8a7913823292
Yash Bandal <147707204+Yash-Bandal@users.noreply.github.com>
2024-10-08 23:44:25 +0530

    Update 9_RotatedSortedArray_2.cpp

--- 9 BinarySearch/9_RotatedSortedArray_2.cpp
+++ 9 BinarySearch/9_RotatedSortedArray_2.cpp
@@ -1,1 +1,45 @@
 
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            //if all same initially
+            if(nums[low] == nums[mid] && nums[mid] == nums[high]){
+           low++;
+                high--;
+                continue;
+            }
+            // consider Left portion is Sorted
+            if (nums[low] <= nums[mid]) {  //equalto condition very imp
+                // consider always first if target is in middle
+                // Traverse only sorted portion
+                if (nums[low] <= target && target <= nums[mid]) { //low -target-mid
+                    // eliminate un nneeded right
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+
+            }
+            // Else right is sorted definately
+            else {
+                if (nums[mid] <= target && target <= nums[high]) { //mid-target-high
+                    // eliminate un nneeded left  
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+};
+
