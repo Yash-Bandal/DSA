0a305f45094b4d93fa8a4ef43571f5e6811661c5
Yash Bandal <147707204+Yash-Bandal@users.noreply.github.com>
2024-10-11 18:24:07 +0530

    Update 1331. Rank Transform of an Array.cpp

--- LeetCode/Arrays/1331. Rank Transform of an Array.cpp
+++ LeetCode/Arrays/1331. Rank Transform of an Array.cpp
@@ -4,7 +4,7 @@ class Solution {
         bool operator()(pair<int, int>& p1, pair<int, int>& p2) {
             return p1.first < p2.first;
         }
-    };
+    }; 
 
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
