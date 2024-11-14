fa4546b43250a3ad62d57b8ebfcb2bd72b8aae24
Yash Bandal <147707204+Yash-Bandal@users.noreply.github.com>
2024-11-14 16:45:21 +0530

    Update 03_PostOrder.cpp

--- 13 BinaryTrees/03_PostOrder.cpp
+++ 13 BinaryTrees/03_PostOrder.cpp
@@ -9,7 +9,7 @@ struct Node {
     int data;
     Node* left;
     Node* right;
-    // Constructor to initialize
+    // Constructor to initialize-
     // the node with a value
     Node(int val) : data(val), left(nullptr), right(nullptr) {}
 };
