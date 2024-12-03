072633d696acf05d4bb78920208ac58759847ab8
Yash Bandal <147707204+Yash-Bandal@users.noreply.github.com>
2024-12-03 16:51:19 +0530

    Create Remove2ForLoops_Optimiz.cpp

---
+++ LeetCode/Important Logics/Remove2ForLoops_Optimiz.cpp
@@ -0,0 +1,55 @@
+class Solution {  //gives TLE
+public:
+    string addSpaces(string s, vector<int>& spaces) {  //spaces=[8,13,15]..directly acces this by maintaining an index..not using double for loop
+        string str = "";
+        int cnt = 0;
+        for (auto c : s) {
+
+            for (int i = 0; i < spaces.size(); i++) {
+                if (cnt == spaces[i]) {
+                    str += " ";
+                }
+            }
+            cnt++;
+            str += c;
+        }
+        return str;
+    }
+};
+
+class Solution {
+public:
+    string addSpaces(string s, vector<int>& spaces) {
+        string str = "";
+        int i = 0;
+        int spcInd = 0;
+        for (auto c : s) {
+
+            if (spcInd < spaces.size() && i == spaces[spcInd]) {    //see this
+                str += " ";
+                spcInd++;
+            }
+            i++;
+            str += c;
+        }
+        return str;
+    }
+};
+
+class Solution {
+public:
+    string addSpaces(string s, vector<int>& spaces) {
+        int n = s.size();
+        string res = "";
+        int ind = 0;
+        for (int i = 0; i < n; i++) {
+
+            if (ind < spaces.size() && i == spaces[ind]) {
+                res += " ";
+                ind++;
+            }
+            res += s[i];
+        }
+        return res;
+    }
+};
