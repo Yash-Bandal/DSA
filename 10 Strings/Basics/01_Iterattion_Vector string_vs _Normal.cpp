83bd670876749047dd5892f8be44b7273b638cf8
Yash Bandal <147707204+Yash-Bandal@users.noreply.github.com>
2024-12-04 23:27:24 +0530

    Create 01_Iterattion_Vector string_vs _Normal.cpp

---
+++ 10 Strings/Basics/01_Iterattion_Vector string_vs _Normal.cpp
@@ -0,0 +1,51 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+string str="Hello this is Yash Bandal";
+for(char c : str){
+    cout<<c<<endl;
+}
+vector<string> str2={"Yash"," ","Bandal"};
+cout<<"using  string" <<endl;
+for(string s : str2){
+    cout<<s<<endl;
+}
+
+    return 0;
+}
+/*
+H
+e
+l
+l
+o
+ 
+t
+h
+i
+s
+ 
+i
+s
+ 
+Y
+a
+s
+h
+ 
+B
+a
+n
+d
+a
+l
+using  string
+Yash
+ 
+Bandal
+
+
+
+*/
