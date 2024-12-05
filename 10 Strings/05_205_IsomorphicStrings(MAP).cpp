8cc4af10b75856c63fe3aac9073cb34925559935
Yash Bandal <147707204+Yash-Bandal@users.noreply.github.com>
2024-12-05 21:19:16 +0530

    Create 05_205_IsomorphicStrings(MAP).cpp

---
+++ 10 Strings/05_205_IsomorphicStrings(MAP).cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char, int> mppS;
+        unordered_map<char, int> mppT;
+
+        for (int i = 0; i < s.length(); i++) {
+            //if == then element check if not present in map
+            //if != then element check if present in map
+            if (mppS.find(s[i]) == mppS.end()) {
+                mppS[s[i]] = i;  //{e : 0 , g :1}
+            }
+
+            if (mppT.find(t[i]) == mppT.end()) {
+                mppT[t[i]] = i; //{a : 0 , d :1}
+            }
+            if (mppS[s[i]] != mppT[t[i]]) { //{1 != 1 ..?} ..or 0!=0..?
+                return false;
+            }
+        }
+        return true;
+    }
+};
