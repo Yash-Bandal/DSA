e71547f9aa9738a456ac55036c05e527d11e4224
Yash Bandal <147707204+Yash-Bandal@users.noreply.github.com>
2024-12-05 21:16:05 +0530

    Create Mappss.cpp

---
+++ 6 Hashing/Basics/Mappss.cpp
@@ -0,0 +1,17 @@
+
+/*
+
+Mpp <key,val>
+
+generally we store value as count default
+or index if we want
+mpp[s[i]] = i
+
+mpp.find(elem) != mpp.end():
+Checks if the element elem exists in the map.
+Returns true if found, false if not.
+  
+mpp.find(elem) == mpp.end():
+Checks if the element elem does not exist in the map.
+Returns true if not found, false if found.
+  */
