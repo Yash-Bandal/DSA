d466fa549b7527dd35813bc3e3965101f570dc42
Yash Bandal <147707204+Yash-Bandal@users.noreply.github.com>
2024-12-16 07:35:10 +0530

    Update and rename 06_876_MidLL.cpp to 06_876_MidLL_Tortoise_Hare.cpp

--- 11 Linked Lists/02_DoublyLinedList_DLL/06_876_MidLL_Tortoise_Hare.cpp
+++ 11 Linked Lists/02_DoublyLinedList_DLL/06_876_MidLL_Tortoise_Hare.cpp
@@ -95,3 +95,23 @@ public:
         return temp;
     }
 };
+
+
+//Tortoise and Hare
+class Solution {
+
+public:
+    ListNode* middleNode(ListNode* head) {
+        if (head == NULL || head->next == NULL) {
+            return head;
+        }
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+};
