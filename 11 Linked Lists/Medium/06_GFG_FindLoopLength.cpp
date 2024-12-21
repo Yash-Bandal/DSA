9d0116d63d1f29550365f6e3c56d4bb93a352364
Yash Bandal <147707204+Yash-Bandal@users.noreply.github.com>
2024-12-21 07:04:18 +0530

    Update 06_GFG_FindLoopLength.cpp

--- 11 Linked Lists/Medium/06_GFG_FindLoopLength.cpp
+++ 11 Linked Lists/Medium/06_GFG_FindLoopLength.cpp
@@ -18,3 +18,34 @@ int countNodesinLoop(Node *head) {
         return 0;
     }
 };
+
+// Optimal 2 pointers
+class Solution {
+private:
+    int findLength(Node* fast, Node* slow) {
+        //Move 1 time..as initially both at same position..so move 1 time 
+        fast = fast->next;
+        int cnt = 1;
+        while (slow != fast) {
+            //next move until again meet
+            fast = fast->next;
+            cnt++;
+        }
+        return cnt;
+    }
+
+public:
+    int countNodesinLoop(Node* head) {
+        Node* fast = head;
+        Node* slow = head;
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) { //when they meey
+                return findLength(fast, slow);
+            }
+        }
+
+        return 0;
+    }
+};
