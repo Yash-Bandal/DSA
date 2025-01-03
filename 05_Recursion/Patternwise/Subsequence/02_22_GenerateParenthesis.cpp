7d3622b21a2c7471793bbcba9901438c3da0f277
Yash Bandal <147707204+Yash-Bandal@users.noreply.github.com>
2025-01-03 22:53:15 +0530

    Update 02_22_GenerateParenthesis.cpp

--- 05_Recursion/Patternwise/Subsequence/02_22_GenerateParenthesis.cpp
+++ 05_Recursion/Patternwise/Subsequence/02_22_GenerateParenthesis.cpp
@@ -60,3 +60,63 @@ generate("", 3, 0)   // Start with empty string
 └── Backtrack
 
 *?
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+private:
+    void generate(vector<string>& res, string str, int open, int close) {
+        // Base case: If no more open or close parentheses to add
+        if (open == 0 && close == 0) {
+            res.push_back(str);
+            return;
+        }
+
+        // Add open parentheses if available
+        if (open > 0) {
+            generate(res, str + "(", open - 1, close);
+        }
+
+        // Add close parentheses if there are more open parentheses added than close ones
+        if (close > open) {
+            generate(res, str + ")", open, close - 1);
+        }
+    }
+
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> res;
+        generate(res, "", n, n); // Start with n open and n close parentheses to add
+        return res;
+    }
+};
+
+int main() {
+    Solution sol;
+    int n;
+    cout << "Enter the value of n: ";
+    cin >> n; // Input the value of n (number of pairs of parentheses)
+
+    vector<string> result = sol.generateParenthesis(n);
+    
+    cout << "Valid Parenthesis combinations for n = " << n << " are:" << endl;
+    for (const string& combination : result) {
+        cout << combination << endl;
+    }
+
+    return 0;
+}
+
+// 
+// Enter the value of n: 3
+// Valid Parenthesis combinations for n = 3 are:
+// ((()))
+// (()())
+// (())()
+// ()(())
+// ()()()
+// 
