9a0572fd2947bfe5434731f6bb572d6216d2f6a9
Yash Bandal <147707204+Yash-Bandal@users.noreply.github.com>
2025-01-03 19:31:01 +0530

    Update Subarray Splits questions LEFTRIGHT  1422 2270.cpp

--- LeetCode/Concepts/Subarray Splits questions LEFTRIGHT  1422 2270.cpp
+++ LeetCode/Concepts/Subarray Splits questions LEFTRIGHT  1422 2270.cpp
@@ -1,35 +1,36 @@
 //1422. Maximum Score After Splitting a String
-// class Solution {
-// public:
-//     int maxScore(string s) {
-//         int sum = 0;
-//         int maxSum = INT_MIN;
-//         int c0 = 0;
-//         int c1 = 0;
-//         int k, j;
+//My tey
+class Solution {
+public:
+    int maxScore(string s) {
+        int sum = 0;
+        int maxSum = INT_MIN;
+        int c0 = 0;
+        int c1 = 0;
+        int k, j;
 
-//         for (int i = 0; i < s.size() - 1; i++) {
-//             // always reset at start of loop not end of loop
-//             c0 = 0;
-//             c1 = 0;
+        for (int i = 0; i < s.size() - 1; i++) {
+            // always reset at start of loop not end of loop
+            c0 = 0;
+            c1 = 0;
 
-//             for (k = 0; k <= i; k++) {
-//                 if (s[k] == '0') {
-//                     c0++;
-//                 }
-//             }
+            for (k = 0; k <= i; k++) {
+                if (s[k] == '0') {
+                    c0++;
+                }
+            }
 
-//             for (j = i + 1; j < s.size(); j++) {
-//                 if (s[j] == '1') {
-//                     c1++;
-//                 }
-//             }
-//             sum = c0 + c1;
-//             maxSum = max(sum, maxSum);
-//         }
-//         return maxSum;
-//     }
-// };
+            for (j = i + 1; j < s.size(); j++) {
+                if (s[j] == '1') {
+                    c1++;
+                }
+            }
+            sum = c0 + c1;
+            maxSum = max(sum, maxSum);
+        }
+        return maxSum;
+    }
+};
 
 // optimizeed
 class Solution {
@@ -46,7 +47,7 @@ public:
             }
         }
 
-        for (int i = 0; i < s.size() - 1; i++) {
+        for (int i = 0; i < s.size() - 1; i++) {  //calculate till n-1 only..dont forget logic..
             if (s[i] == '0') {
                 c0++;
             } else if (s[i] == '1') {
