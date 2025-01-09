392e418d891a9049150d9ad877781737037178b4
Yash Bandal <147707204+Yash-Bandal@users.noreply.github.com>
2025-01-09 18:17:13 +0530

    Create 09_2185. Counting Words With a Given Prefix.cpp

---
+++ LeetCode/POTD/January 2025/09_2185. Counting Words With a Given Prefix.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int prefixCount(vector<string>& words, string pref) {
+        int cnt = 0;
+        for (auto it : words) {
+            if (it.find(pref) == 0) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
+
+//=====================================================================
+
+class Solution {
+public:
+    int prefixCount(vector<string>& words, string pref) {
+        int c = 0;
+        int n = pref.length();
+        for (const string& w : words) {
+            if (w.size() >= n && w.compare(0, n, pref) == 0) {
+                c++;
+            }
+        }
+        return c;
+    }
+};
